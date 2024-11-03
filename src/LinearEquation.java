e5416e21aaad67503951c4a3b96709815a260b4f
Zeosand <zeosandl@nycstudents.net>
2024-11-03 16:36:32 -0500

    submit

--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -1,2 +1,47 @@
-public class LineEquation {
+public class LinearEquation {
+    private int x1;
+    private int y1;
+    private int x2;
+    private int y2;
+
+
+    public LinearEquation(int x1, int y1, int x2, int y2){
+        this.x1 = x1;
+        this.y1 = y1;
+        this.x2 = x2;
+        this.y2 = y2;
+    }
+    public double distance(){
+        return roundedToHundredth(Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2)));
+    }
+    public double yIntercept(){
+        return y1 - ((double) (y2 - y1) / (x2 - x1) * x1);
+    }
+    public double slope(){
+        double x =  (x2 - x1);
+        double y = (y2 - y1);
+        return roundedToHundredth(y/x);
+    }
+    public String equation(){
+        if (y1 == y2){
+            return "y = " + yIntercept();
+        }
+        return "y = " + (y2 - y1) + "/" + (x2 - x1) + "x" + " + " +  yIntercept();
+    }
+    public String coordinateForX(double x){
+        System.out.println();
+        double y = slope() * x + yIntercept();
+        return "(" + x + ", " + y + ")";
+    }
+    public String lineInfo(){
+        return ("The two points are: " + "("+ x1 + ", " + y1 + ")" + " and " + "("+ x2 + ", " + y2 + ")") + "\n" +
+                ("The equation of the line between these points is: " + equation()) + "\n" +
+                ("The y-intercept of the line is: " + yIntercept()) + "\n" +
+                ("The slope of this line is: " + slope()) + "\n" +
+                ("The distance between the two points is: " + distance());
+    }
+    private double roundedToHundredth(double toRound){
+        return Math.round(toRound * 100) / 100.0;
+    }
+
 }
--- src/LinearEquationLogic.java
+++ src/LinearEquationLogic.java
@@ -1,4 +1,47 @@
-package PACKAGE_NAME;
-
+import java.util.Scanner;
 public class LinearEquationLogic {
+    private Scanner scan;
+    private LinearEquation linearEquation;
+    public LinearEquationLogic() {
+        scan = new Scanner(System.in);
+    }
+
+
+    public void start() {
+            getCoordinates();
+    }
+
+
+    private void getCoordinates() { //helper
+        String restart = "y";
+        System.out.println("Welcome to the linear equation calculator!");
+        while (restart.equals("y")) {
+        System.out.print("Enter coordinate 1: ");
+        String cord1 = scan.nextLine();
+        System.out.print("Enter coordinate 2: ");
+        String cord2 = scan.nextLine();
+        int x1 = Integer.parseInt(cord1.substring(1, cord1.indexOf(",")));
+        int y1 = Integer.parseInt(cord1.substring(cord1.indexOf(",") + 2, cord1.indexOf(")")));
+        int x2 = Integer.parseInt(cord2.substring(1, cord2.indexOf(",")));
+        int y2 = Integer.parseInt(cord2.substring(cord2.indexOf(",") + 2, cord2.indexOf(")")));
+        linearEquation = new LinearEquation(x1, y1, x2, y2);
+        if (x1 == x2) {
+            System.out.println();
+            System.out.println("These points are on a vertical line: x = " + x1);
+            System.out.println();
+        } else {
+            System.out.println();
+            System.out.println(linearEquation.lineInfo());
+            System.out.println();
+            System.out.print("Enter a value for x: ");
+            double x = scan.nextDouble();
+            scan.nextLine();
+            System.out.println("The point on the line is " + linearEquation.coordinateForX(x));
+        }
+        System.out.println();
+        System.out.print("Would you like you enter another pair of coordinates? y/n: ");
+        restart = scan.nextLine().toLowerCase();
+        }
+        System.out.println("Thank you for using the slope calculator, goodbye!");
+    }
 }
